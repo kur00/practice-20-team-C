29fb48d79dd1ab4aa7a687b1d0bbac7279d9b03c
oiru777 <warisara11037@gmail.com>
2025-01-29 10:29:50 +0900

    課題17

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\CompanyController;
 
@@ -12,3 +13,10 @@ Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/{id}', [PostController::class, 'show'])->name('post.show');
 
 Route::get('/companies', [CompanyController::class, 'index'])->name('companies.index');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/dashboard', function() {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
