0c70b580fb5098b59a520c226bef6bee051aaece
oiru777 <warisara11037@gmail.com>
2025-02-01 19:44:11 +0900

    タグをデータベースに保存

--- app/Http/Controllers/Testcontroller.php
+++ app/Http/Controllers/Testcontroller.php
@@ -1,10 +1,21 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Models;
 
-use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Testcontroller extends Controller
+class Tag extends Model
 {
-    //
+    use HasFactory;
+
+    // タグモデルの設定
+    protected $fillable = ['name']; // nameカラムを一括代入できるように設定
+
+    // 投稿との多対多のリレーション
+    public function posts()
+    {
+        return $this->belongsToMany(Post::class); // 投稿との多対多リレーション
+    }
 }
+
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -15,11 +15,15 @@ class Post extends Model
     //コメントのリレーション
     public function comments(): HasMany
     {
-        return $this->hasMany(Comment::class);
+        return $this->hasMany(Comment::class);  // 1つの投稿は複数のコメントを持つ
     }
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class); // 各投稿は1人のユーザーに属する
+    }
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class); // 投稿は複数のタグを持つ、タグは複数の投稿を持つ
     }
 
 }
